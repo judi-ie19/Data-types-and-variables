29fbd5d0576f7424ec36c10e038341575cf6fd3e
jud-ie19 <owuorakinyijudith@gmail.com>
2022-02-12 08:13:13 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name = "judy"
+    println(name)
+    var age = "21"
+    println(age)
+    var number = "0757497424"
+    println(number)
+    var weight = "51"
+    println(weight)
+    val iskenyan: Boolean = true
+    val isnotkenyan: Boolean = false
+    println(iskenyan) // output is true
+    println(iskenyan)// output is false
+}
+
+
